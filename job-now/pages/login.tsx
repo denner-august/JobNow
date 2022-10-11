b2e241a93de1481580fe91a9d50d75a82c2fe8d1
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-10-10 21:48:02 -0300

    estilizando pagina de login

--- job-now/pages/login.tsx
+++ job-now/pages/login.tsx
@@ -1,4 +1,42 @@
 import styles from "../styles/login.module.scss";
+import Image from "next/image";
+
 export default function Login() {
-  return <div className={styles.Container}></div>;
+  const TamanhoIcons = {
+    width: 50,
+    heigth: 50,
+  };
+
+  return (
+    <div className={styles.Container}>
+      <div className={styles.loginform} id={styles.Empregador}>
+        <div className={styles.Titulo}>
+          <p>Empresa</p>
+        </div>
+        <div className={styles.EntrarEmpresa}>
+          <Image
+            className={styles.Linkedin}
+            src="/images/linkedin.png"
+            alt="Picture of the author"
+            width={TamanhoIcons.width}
+            height={TamanhoIcons.width}
+          />
+        </div>
+      </div>
+      <div className={styles.loginform} id={styles.Programador}>
+        <div className={styles.Titulo}>
+          <p>Programador</p>
+        </div>
+        <div className={styles.EntrarProgramador}>
+          <Image
+            className={styles.Github}
+            src="/images/github.png"
+            alt="Picture of the author"
+            width={TamanhoIcons.width}
+            height={TamanhoIcons.width}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }

9efd8f1fd04d866b2e9cd9e164f1dd5619dcda87
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-10-17 13:27:19 -0300

    Update login.tsx

--- job-now/pages/login.tsx
+++ job-now/pages/login.tsx
@@ -10,19 +10,16 @@ export default function Login() {
   const { data: session } = useSession();
   const router = useRouter();
 
-  const linkedinImage = "/images/linkedin.png";
-  const githubImage = "/images/github.png";
-
   const TamanhoIcons = {
     width: 50,
     heigth: 50,
   };
 
-  // useEffect(() => {
-  //   if (session) {
-  //     router.push("user");
-  //   }
-  // }, [router, session]);
+  useEffect(() => {
+    if (session) {
+      router.push("user");
+    }
+  }, [router, session]);
 
   useEffect(() => {
     console.log(session);
@@ -56,35 +53,6 @@ export default function Login() {
           height={TamanhoIcons.width}
         />
       </button>
-
-      {/* <div className={styles.loginform} id={styles.Empregador}>
-        <div className={styles.Titulo}>
-          <p>Empresa</p>
-        </div>
-        <button className={styles.EntrarEmpresa}>
-          <LoginButton
-            className={styles.Linkedin}
-            src="/images/linkedin.png"
-            alt="Picture of the author"
-            width={TamanhoIcons.width}
-            height={TamanhoIcons.width}
-          />
-        </button>
-      </div>
-      <div className={styles.loginform} id={styles.Programador}>
-        <div className={styles.Titulo}>
-          <p>Programador</p>
-        </div>
-        <button className={styles.EntrarProgramador} onClick={Logar}>
-          <LoginButton
-            className={styles.Github}
-            src="/images/github.png"
-            alt="Picture of the author"
-            width={TamanhoIcons.width}
-            height={TamanhoIcons.width}
-          />
-        </button>
-      </div> */}
     </div>
   );
 }

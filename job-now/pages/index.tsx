150bb2b0746f808cf4713f8d34a760a742293d7c
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-10-16 22:39:58 -0300

    Update index.tsx

--- job-now/pages/index.tsx
+++ job-now/pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from "next";
 import styles from "../styles/home.module.scss";
+import Swal from "sweetalert2";
 
 import { PrincipalHome } from "../components/homePage/principal";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    Swal.fire("Este é um projeto ainda em fase de desenvolvimento");
+  }, []);
+
   return (
     <div className={styles.Container}>
       <PrincipalHome />

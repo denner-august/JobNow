c69a24c0f75f46a9bf63286702eb8d280b50efa9
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-10-19 13:08:43 -0300

    Update user.tsx

--- job-now/pages/user.tsx
+++ job-now/pages/user.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";
+// import Image from "next/image";
+import Image from "next/future/image";
+
 import styles from "../styles/user.module.scss";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useEffect } from "react";
 import Router from "next/router";
+import { Div } from "../styles/components/photo";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+import "swiper/css";
 
 export default function User() {
   const { data: session } = useSession();
@@ -44,6 +52,47 @@ export default function User() {
           <p>{session?.user?.name}</p>
         </button>
       </header>
+
+      <div className={styles.Container2}>
+        <Div className={styles.photo} usuario={session?.user?.image}></Div>
+        <div className={styles.Menu}>
+          <div className={styles.carrosel}>
+            <Swiper
+              style={{
+                height: "100%",
+                width: "100%",
+              }}
+              slidesPerView={1}
+              loop={true}
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              modules={[Autoplay]}
+            >
+              <SwiperSlide>
+                <Image src="/images/login/dusty-rose.jpg" alt="rosa" fill />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image
+                  src="/images/login/long-forest-path.jpg"
+                  alt="rosa"
+                  fill
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image
+                  src="/images/login/white-rose-close-up.jpg"
+                  alt="rosa"
+                  fill
+                />
+              </SwiperSlide>
+            </Swiper>
+          </div>
+
+          <div className={styles.options}></div>
+        </div>
+      </div>
     </div>
   );
 }

a3ad2a7f292df5c02a0efb322d9845275fa004c5
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-11-09 17:14:16 -0300

    Update user.tsx

--- job-now/pages/user.tsx
+++ job-now/pages/user.tsx
@@ -1,16 +1,12 @@
-// import Image from "next/image";
 import Image from "next/future/image";
 
 import styles from "../styles/user.module.scss";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 import { useEffect } from "react";
 import Router from "next/router";
-import { Div } from "../styles/components/photo";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper";
-
-import "swiper/css";
+import Jobs from "../jobs/jobs.json";
+import { jobProps } from "./types/jobs";
 
 export default function User() {
   const { data: session } = useSession();
@@ -53,45 +49,40 @@ export default function User() {
         </button>
       </header>
 
-      <div className={styles.Container2}>
-        <Div className={styles.photo} usuario={session?.user?.image}></Div>
-        <div className={styles.Menu}>
-          <div className={styles.carrosel}>
-            <Swiper
-              style={{
-                height: "100%",
-                width: "100%",
-              }}
-              slidesPerView={1}
-              loop={true}
-              autoplay={{
-                delay: 3000,
-                disableOnInteraction: false,
-              }}
-              modules={[Autoplay]}
-            >
-              <SwiperSlide>
-                <Image src="/images/login/dusty-rose.jpg" alt="rosa" fill />
-              </SwiperSlide>
-              <SwiperSlide>
+      <div className={styles.ContainerJob}>
+        {Jobs.vagas.map((items: jobProps, index) => {
+          return (
+            <div className={styles.layoutVagas} key={items.detalhes}>
+              <div className={styles.titulo}>
                 <Image
-                  src="/images/login/long-forest-path.jpg"
-                  alt="rosa"
-                  fill
+                  src={items.logo}
+                  alt="imagem do usuario"
+                  width={200}
+                  height={200}
                 />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image
-                  src="/images/login/white-rose-close-up.jpg"
-                  alt="rosa"
-                  fill
-                />
-              </SwiperSlide>
-            </Swiper>
-          </div>
 
-          <div className={styles.options}></div>
-        </div>
+                <p>Titulo: {items.titulo}</p>
+              </div>
+
+              <div className={styles.vagaDados}>
+                <p> Experiencia: {items.experiencia}</p>
+                <p> Salario: {items.salario}</p>
+                <p> Modelo: {items.tipo}</p>
+              </div>
+
+              <ul>
+                <p>Tecnologias </p>
+                {items.tecnologias.map((tec, index) => {
+                  return <li key={index}>{tec}</li>;
+                })}
+              </ul>
+
+              <div className={styles.buttonLayout}>
+                <button>Ver Vaga</button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );

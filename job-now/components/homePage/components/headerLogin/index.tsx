1e692e83fed0a93b44cb3e69650934790a94e09f
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-11-14 16:45:39 -0300

    Create index.tsx
    component header após o login

---
+++ job-now/components/homePage/components/headerLogin/index.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { useContext } from "react";
+import { Context } from "../../../../context/userContext";
+
+import styles from "./headerDefault.module.scss";
+
+import Router from "next/router";
+import { signOut } from "next-auth/react";
+
+export function HeaderDefaul() {
+  const { user } = useContext(Context);
+
+  function Logout() {
+    if (user) {
+      signOut();
+      Router.push("/");
+    }
+    Router.push("/");
+  }
+
+  function PushInicio() {
+    Router.push("user");
+  }
+
+  return (
+    <div className={styles.Container}>
+      <header className={styles.header}>
+        <button className={styles.button} onClick={PushInicio}>
+          Inicio
+        </button>
+
+        <button
+          className={styles.button}
+          onClick={() => Router.push("CreateJob")}
+        >
+          criar vaga
+        </button>
+
+        <button className={styles.perfil} onClick={Logout}>
+          <Image
+            src="/images/github2.png"
+            alt="imagem do usuario"
+            width={25}
+            height={25}
+          />
+          <p>{user?.name}</p>
+        </button>
+      </header>
+    </div>
+  );
+}

57ef93555386efc5aec8a21da98c4ef6a7c2790d
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-11-23 17:56:35 -0300

    Update CreateJob.tsx

--- job-now/pages/CreateJob.tsx
+++ job-now/pages/CreateJob.tsx
@@ -1,12 +1,8 @@
 import { HeaderDefaul } from "../components/homePage/components/headerLogin";
 
-import { useForm } from "react-hook-form";
 import { CreateJobForm } from "../components/homePage/components/formCreateJob/createJobForm";
 
 export default function CreateJob() {
-  // falta colocar a logo da empresa como input
-  // tipar os dados do form
-
   return (
     <>
       <HeaderDefaul />

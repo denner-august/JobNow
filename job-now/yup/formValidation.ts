5acf146b4791e00fb225ef33c17c14640c793956
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-11-30 18:08:52 -0300

    Create formValidation.ts

---
+++ job-now/yup/formValidation.ts
@@ -0,0 +1,25 @@
+import * as yup from "yup";
+
+export const schema = yup
+  .object({
+    TituloVaga: yup.string().required("Escreva o titulo da vaga"),
+    Detalhes: yup.string().required("Escrava os detalhes da vaga"),
+    tipo: yup.string().required("qual o tipo da vaga?"),
+    Salario: yup
+      .number()
+      .required("Qual o salario para essa vaga?")
+      .typeError("Qual o salario para essa vaga?"),
+    Tecnologias: yup
+      .array(
+        yup.object({
+          linguagem: yup.string().required("linguagem ncessario"),
+        })
+      )
+      .required(),
+    emailVaga: yup
+      .string()
+      .email("Digite um email valido")
+      .required("um email para receber os curriculos é necessario"),
+  })
+  .required()
+  .required();

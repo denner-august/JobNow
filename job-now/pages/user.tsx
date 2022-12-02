f9047947019e951d27f39026e9ac7b3a6698f6a1
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-12-01 22:57:01 -0300

    Update user.tsx
    Este component foi isolado

--- job-now/pages/user.tsx
+++ job-now/pages/user.tsx
@@ -1,14 +1,14 @@
 import { useContext, useEffect } from "react";
 import Router from "next/router";
-import styles from "../styles/user.module.scss";
 import { Context } from "../context/userContext";
 
-import Image from "next/future/image";
+import styles from "../styles/user.module.scss";
+
+import { HeaderDefaul } from "../components/homePage/components/headerLogin/index";
+import { ExibiVaga } from "../components/homePage/components/exibiVaga";
 
 import Jobs from "../jobs/jobs.json";
-
 import { jobProps } from "../types/jobs";
-import { HeaderDefaul } from "../components/homePage/components/headerLogin/index";
 
 export default function User() {
   const { user } = useContext(Context);
@@ -21,49 +21,13 @@ export default function User() {
     }
   }, [user]);
 
-  function pushViewJob(id: number) {
-    Router.push(`/ViewJob/${id}`);
-  }
-
   return (
     <div className={styles.Container}>
       <HeaderDefaul />
 
-      <div className={styles.ContainerJob}>
-        {Jobs.vagas.map((items: jobProps) => {
-          return (
-            <div className={styles.layoutVagas} key={items.detalhes}>
-              <div className={styles.titulo}>
-                <Image
-                  src={items.logo}
-                  alt="imagem do usuario"
-                  width={200}
-                  height={200}
-                />
-
-                <p>Titulo: {items.titulo}</p>
-              </div>
-
-              <div className={styles.vagaDados}>
-                <p> Experiencia: {items.experiencia}</p>
-                <p> Salario: {items.salario}</p>
-                <p> Modelo: {items.tipo}</p>
-              </div>
-
-              <ul>
-                <p>Tecnologias </p>
-                {items.tecnologias.map((tec, index) => {
-                  return <li key={index}>{tec}</li>;
-                })}
-              </ul>
-
-              <div className={styles.buttonLayout}>
-                <button onClick={() => pushViewJob(items.id)}>Ver Vaga</button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      {Jobs.vagas.map((items: jobProps) => {
+        return <ExibiVaga job={items} />;
+      })}
     </div>
   );
 }

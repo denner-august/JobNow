fbdeb636875b7bdc7a9e12eed8b3d629ba5283ff
denner augusto <denner.augusto90@gmail.com>
2022-12-13 15:05:13 -0300

    Update perfil.tsx

--- job-now/pages/perfil.tsx
+++ job-now/pages/perfil.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { ExibiVaga } from "../components/homePage/components/exibiVaga";
 import { HeaderDefaul } from "../components/homePage/components/headerLogin";
 
+import Styles from "../styles/perfil.module.scss";
+
 import styles from "../styles/user.module.scss";
 
 import { jobProps } from "../types/jobs";
@@ -9,15 +11,35 @@ import Jobs from "../jobs/jobs.json";
 
 export default function Perfil() {
   const [jobsUser, setJObsUser] = useState(() => {
-    return Jobs.vagas.filter((item) => item.id === 2);
+    return Jobs.vagas.filter((item) => item.id === 1);
   });
+
+  function MostraVagas() {
+    return jobsUser.map((items: jobProps) => {
+      return (
+        <ExibiVaga
+          key={Math.random()}
+          job={items}
+          buttonNameVaga="Deletar"
+          deletarVaga={true}
+        />
+      );
+    });
+  }
+
+  function SemVaga() {
+    return (
+      <div className={Styles.Container}>
+        <p>Você não criou nenhuma vaga</p>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.Container}>
       <HeaderDefaul />
-      {jobsUser.map((items: jobProps) => {
-        return <ExibiVaga key={Math.random()} job={items} />;
-      })}
+
+      {jobsUser.length > 0 ? MostraVagas() : SemVaga()}
     </div>
   );
 }

3c693062b68c5b26fb4325add804e047ae9f4b9f
denner augusto <denner.augusto90@gmail.com>
2022-12-18 22:29:35 -0300

    Update user.tsx

--- job-now/pages/user.tsx
+++ job-now/pages/user.tsx
@@ -1,15 +1,15 @@
-import { useContext, useEffect } from "react";
+import { useEffect, useState } from "react";
 import Router from "next/router";
-import { Context } from "../context/userContext";
 
 import styles from "../styles/user.module.scss";
 
 import { HeaderDefaul } from "../components/homePage/components/headerLogin/index";
 import { ExibiVaga } from "../components/homePage/components/exibiVaga";
 
-import Jobs from "../jobs/jobs.json";
 import { jobProps } from "../types/jobs";
 import { useSession } from "next-auth/react";
+import { getJobs } from "../requests/methods";
+import { CreateJobFormProps } from "../types/createJobForm";
 
 export default function User() {
   const { status } = useSession();
@@ -21,12 +21,29 @@ export default function User() {
   if (status === "unauthenticated") {
     Router.push("/login");
   }
+  const [Jobs, setJobs] = useState<CreateJobFormProps[]>([]);
+
+  useEffect(() => {
+    async function allJobs() {
+      const data = await getJobs();
+
+      setJobs(data.vagas);
+    }
+
+    allJobs();
+
+    return setJobs([]);
+  }, [status]);
+
+  if (Jobs.length === 0) {
+    return <h1>Carregando</h1>;
+  }
 
   return (
     <div className={styles.Container}>
       <HeaderDefaul />
 
-      {Jobs.vagas.map((items: jobProps) => {
+      {Jobs.map((items: CreateJobFormProps) => {
         return (
           <ExibiVaga
             key={Math.random()}

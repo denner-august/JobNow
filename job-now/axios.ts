be27ef0b67c2701a4df6fe251119b2cce79074d4
denner augusto <denner.augusto90@gmail.com>
2022-12-23 14:29:58 -0300

    Update axios.ts

--- job-now/axios.ts
+++ job-now/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const token = localStorage.getItem("Auth");
+
 export const api = axios.create({
   baseURL: "http://localhost:3001",
+  headers: token ? { Bearer: token } : undefined,
 });

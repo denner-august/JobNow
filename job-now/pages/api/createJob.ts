4aa5bdea145b85d0d2c634217ea358f53e7faab7
denner augusto <denner.augusto90@gmail.com>
2023-01-02 16:01:31 -0300

    criando metodos

---
+++ job-now/pages/api/createJob.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  if (method !== "POST") {
+    return res.status(400).json({ resposta: "methodo errado" });
+  }
+
+  //função que cria um novo trabalho
+};

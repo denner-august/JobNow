5e246585dd22b3327c09c5609c56ed2cb62e68de
denner augusto <denner.augusto90@gmail.com>
2023-01-06 14:25:46 -0300

    Update createJob.ts

--- job-now/requests/createJob.ts
+++ job-now/requests/createJob.ts
@@ -1,6 +1,7 @@
-import { client, q } from "../faunadb/faunaConfig";
+import { client, q } from "../pages/api/faunaConfig";
+import { CreateJobFormProps } from "../types/createJobForm";
 
-export function Createjob(data: any) {
+export function Createjob(data: CreateJobFormProps) {
   return client.query(q.Create(q.Collection("jobs"), { data }));
 }
 //função que cria um novo trabalho

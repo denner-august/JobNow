8212e26fe1a9d5ba17fe6aa8fbae65b198128f93
denner augusto <denner.augusto90@gmail.com>
2023-01-09 19:02:43 -0300

    Update faunaConfig.ts

--- job-now/pages/api/faunaConfig.ts
+++ job-now/pages/api/faunaConfig.ts
@@ -2,5 +2,6 @@ import faundb from "faunadb";
 export const q = faundb.query;
 
 export const client = new faundb.Client({
-  secret: `${process.env.FAUNA_SECRET}`,
+  // secret: `${process.env.FAUNA_SECRET}`,
+  secret: "fnAE5kB-a8ACT9OCDGhA-G3ulBWJp1JvVOE6tI17",
 });

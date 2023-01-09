a757a7bdd2e5e66051484ee662f526ed7dc44974
denner augusto <denner.augusto90@gmail.com>
2023-01-09 19:02:49 -0300

    Create allJobOneUser.ts

---
+++ job-now/pages/api/allJobOneUser.ts
@@ -0,0 +1,28 @@
+import { CreateJobFormProps } from "../../types/createJobForm";
+import { client, q } from "./faunaConfig";
+
+type dataProps = {
+  data: CreateJobFormProps;
+};
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+  const { email } = req.body;
+
+  if (method !== "POST") {
+    return res.status(400).json({ resposta: "methodo errado" });
+  }
+
+  const request: dataProps = await client.query(
+    q.Map(
+      q.Paginate(q.Match(q.Index("jobUserEmail"), [email])),
+      q.Lambda((x) => q.Get(x))
+    )
+  );
+
+  return res.status(200).json(request.data);
+};
+
+//função que cria um novo trabalho

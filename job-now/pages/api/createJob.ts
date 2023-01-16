b1e20ca54cb625ceef69080e330443f66b19a484
denner augusto <denner.augusto90@gmail.com>
2023-01-16 13:47:33 -0300

    Update createJob.ts

--- job-now/pages/api/createJob.ts
+++ job-now/pages/api/createJob.ts
@@ -7,15 +7,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
   const { data } = req.body;
 
-
   if (method !== "POST") {
     return res.status(400).json({ resposta: "methodo errado" });
   }
 
-  const linguangens: any = data.Tecnologias.flatMap(
-    (linguagens: any) => linguagens.linguagem
-  );
-  data.Tecnologias = linguangens;
 
   try {
     client.query(q.Create(q.Collection("jobs"), { data }));
